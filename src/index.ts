e39ae300bebbb59e19208985538bd7ffd2bee270
Kyrylo Avramenko <wkyrylo@gmail.com>
2021-10-03 14:44:26 -0700

    add listing endpoint

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,15 @@ import express, { Request, Response } from 'express';
 require('dotenv').config();
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
+});
+
+app.route('/listing').get(function (req: Request, res: Response) {
+  const url = req.body['url'];
+  res.json({ result: `your url ${url}` });
 });
 
 const PORT = process.env.PORT || 5000;
